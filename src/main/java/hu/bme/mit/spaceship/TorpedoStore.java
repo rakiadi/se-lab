5084c44d19194bd190648844e11aa563181d803e
rakiadi <144821792+rakiadi@users.noreply.github.com>
2023-11-08 16:42:07 +0100

    Update TorpedoStore.java

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -8,7 +8,7 @@ import java.util.Random;
 * (Deliberately contains bugs.)
 */
 public class TorpedoStore {
-
+  //private Random generator = new Random();
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
 
@@ -36,7 +36,7 @@ public class TorpedoStore {
     boolean success = false;
 
     
-    double random = new Random();  // Compliant
+    double r = new Random();  // Compliant
   
       
   
